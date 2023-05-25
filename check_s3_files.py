b93241e84a4ff41ee1ce643528000c60cf79411f
HapizTeoh <hapizteoh99@gmail.com>
2023-05-25 17:54:14 +0800

    Add SNS publish code

--- check_s3_files.py
+++ check_s3_files.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.exceptions import ClientError
 import datetime
 from datetime import timedelta, date
 import logging,re, os
@@ -15,14 +16,20 @@ boto3.setup_default_session(profile_name=os.getenv("PROFILE_NAME"))
 #Setup boto3 client
 s3_client = boto3.client('s3')
 
+#Setup region
+region = os.getenv("REGION")
+
 #the number of days an object will be considered expired
 expire_threshold = int(os.getenv("EXPIRE_THRESHOLD"))
 
 #the total number of s3 objects that will be removed
 total_objects = 0
 
+expiring_folders = []
+
 #bucket Name
 bucket_name = os.getenv("BUCKET_NAME")
+s3_lifecycle = os.getenv("S3_LIFECYCLE")
 
 #Get 14 days ahead of today
 date_in_two_weeks = date.today() + timedelta(days=14)
@@ -43,6 +50,7 @@ def check_number_of_objects(folder_names):
 
 def check_subdirectories_older_than_date(s3client,logs_file, cut_date):
     global total_objects
+    global expiring_folders
     prefix = logs_file+"/"
     result = s3client.list_objects(Bucket=bucket_name, Prefix=prefix, Delimiter='/')
     for o in result.get('CommonPrefixes'):
@@ -59,21 +67,58 @@ def check_subdirectories_older_than_date(s3client,logs_file, cut_date):
             if dates_in_log_folder_object.date() <= cut_date:
                 num_of_files = check_number_of_objects(subfolder)
                 logging.info(f"This folder will be expired by {cut_date} : {subfolder}, which contains {num_of_files} objects" )
+                expiring_folders.append(subfolder)
                 total_objects += num_of_files
         else:
             logging.info(f"No folders with dates found in {logs_file}")
+            
+def publish_message(topic_arn, message, subject, region):
+    """
+    Publishes a message to a topic.
+    """
+    AWS_REGION = region
+    sns_client = boto3.client('sns', region_name=AWS_REGION)
+    
+    try:
+        response = sns_client.publish(
+            TopicArn=topic_arn,
+            Message=message,
+            Subject=subject,
+        )['MessageId']
+    except ClientError:
+        logging.exception(f'Could not publish message to the topic.')
+        raise
+    else:
+        return response
 
-# Get the content of logs.directory and get the files
-with open("logs_directory.txt") as f:
-    logs_directory = [line.rstrip() for line in f]
+if __name__ == "__main__":
+    # Get the content of logs.directory and get the files
+    with open("logs_directory.txt") as f:
+        logs_directory = [line.rstrip() for line in f]
+        
+    # Loop through all the logs directories in S3
+    for log_type in logs_directory:
+        s3client = s3_client
+        logging.info(f"Checking the folders inside {log_type}")
+        check_subdirectories_older_than_date(s3client,log_type,cut_date)
 
-# Loop through all the logs directories in S3
-for log_type in logs_directory:
-    s3client = s3_client
-    logging.info(f"Checking the folders inside {log_type}")
-    check_subdirectories_older_than_date(s3client,log_type,cut_date)
-
-#Total objects will be expired in the next 14 days
-logging.info(f"A total of {total_objects:,} objects will be expired by {cut_date}")
-
-## Will send msg to SNS 
+    #Total objects will be expired in the next 14 days
+    logging.info(f"A total of {total_objects:,} objects will be expired by {cut_date}")
+    
+    #Send SNS message
+    topic_arn = os.getenv("TOPIC_ARN")
+    message = f'This is a notification about logs files deletion.\n' \
+            f'{total_objects:,} objects will be removed by {cut_date} (14 days from today).\n' \
+            f'These folders and its content will be expiring by {cut_date} : \n'
+    
+    for folders in expiring_folders:
+        message += folders + "\n"
+        
+    message += f'Checkout the bucket S3 lifecycle here {s3_lifecycle}.'
+    
+    subject = 'Logs will be removed in S3 logs [TESTING]'
+    logging.info(f'Publishing message to topic - {topic_arn}...')
+    message_id = publish_message(topic_arn, message, subject,region)
+    logging.info(
+        f'Message published to topic - {topic_arn} with message Id - {message_id}.'
+    )
